536982d5a9ea5f07cc53d3bad8a8fa90204c70ad
Eduardo Magaldi <eduardomagaldi@192.168.1.3>
2017-02-21 03:04:10 -0300

    bootstraping project and adding first html and css

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp')
+	// ejs = require('gulp-ejs'),
+	stylus = require('gulp-stylus'),
+	connect = require('gulp-connect');
+
+// gulp.task('ejs', function() {
+// 	gulp.src(['./src/templates/views/**/*.ejs', './src/templates/*.ejs'])
+// 		.pipe(ejs({
+// 				msg: 'Hello Gulp 2!'
+// 			},
+// 			{
+// 				ext: '.html'
+// 			})
+// 		)
+// 		.pipe(gulp.dest('./public'));
+// });
+
+gulp.task('css', function () {
+	return gulp.src('./src/css/main.styl')
+		.pipe(stylus())
+		.pipe(gulp.dest('./public/css/'));
+});
+
+gulp.task('watch', function () {
+	gulp.watch(['./src/css/**/*'], ['process']);
+});
+ 
+gulp.task('webserver', function () {
+	connect.server({
+		name: 'app',
+		root: ['public', 'node_modules'],
+		port: 8000,
+		livereload: true
+	});
+});
+
+gulp.task('process', ['css']);
+gulp.task('default', ['process', 'watch', 'webserver']);
+// gulp.task('default', ['webserver']);
